fdedc9b71339a60dca894bed71997a2eb4a9901d
niujianlong <niu.jianlong@o-film.com>
2016-09-19 13:43:21 +0800

    backup

---
+++ C51EDBCAnalysis/C51EdbcSort.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+import os
+Base_Dir = os.path.dirname(__file__)
+
+fw = file(os.path.join(Base_Dir,'GenFile//SourceGenFile',"C51E48Sorted.txt"), "w+")
+frameDict = {}
+frameList = []
+sortedFrameList = []
+i = -1
+for line in open(os.path.join(Base_Dir,'SourceFile',"C51E49WithTestID.txt")):  
+    line_split = line.split(' ')
+    if line_split[0] == 'BO_':
+        frameID=int(line_split[1])
+        frameList.append(line)
+        i = i + 1
+    elif line_split[0] == '':
+        frameList.append(line)
+    else: 
+        frameDict[frameID]= frameList
+        frameID = 0
+        frameList = []    
+       
+sortedFrameList = sorted(frameDict.iteritems(),key = lambda asd:asd[0],reverse = False)
+k=0
+for tupleIndex in sortedFrameList:
+    if k != 0:
+        fw.write('\n')
+    for BOandSG in tupleIndex[1]:
+        print BOandSG
+        fw.write(BOandSG)
+        k = k+1
+fw.write('\n')        
