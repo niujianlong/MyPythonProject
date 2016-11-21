99b283d1f77e31a90c49553a98dc44e59adb76f5
niujianlong <niu.jianlong@o-film.com>
2016-11-21 14:30:25 +0800

    完成了所有功能并且可以自动拉取工程 目前还存在卡住的现象 需要进一步分析

--- AutomaticRepoGitProjectFetch/src/AutoFetch.py
+++ AutomaticRepoGitProjectFetch/src/AutoFetch.py
@@ -7,6 +7,7 @@ Created on 2016年11月18日
 '''
 import os
 import subprocess 
+import time
 
 def cygwin(command):
     """
@@ -21,16 +22,21 @@ def cygwin(command):
     # Make sure Cygwin binary directory in path
     if cygwin_bin not in os.environ['PATH']:
         os.environ['PATH'] += ';' + cygwin_bin
+    #print cygwin_bin   
     # Launch Bash
     p = subprocess.Popen(
         args=['bash', '--login', '-c', command],
         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    timebefore = time.time()
     p.wait()
+    timeafter = time.time()
+    
+    print 'Total Time Spent = '+str(timeafter - timebefore)+'s'
     # Raise exception if return code indicates error
     if p.returncode != 0:
         raise RuntimeError(p.stderr.read().rstrip())
     # Remove trailing newline from output
-    print (p.stdout.read() + p.stderr.read()).rstrip()
+    #print (p.stdout.read() + p.stderr.read()).rstrip()
     p.kill()
 
 # os.chdir(ur'E:\AllProjectGit\B100KIOC')
@@ -44,24 +50,26 @@ def cygwin(command):
 
 
 # os.chdir(ur'E:\AllProjectGit\B100KIOC')
-
-
+#print os.environ['PATH']
+TaskStarttime = time.time()
 print '---------------------------------------------------------------------------\n repo sync start!'    
-'''
-print cygwin('cd E:AllProjectGit/B100KIOC ; rm -rf *; repo sync')
-print cygwin('cd E:AllProjectGit/B100KIOC-gas ; rm -rf *; repo sync')
-print cygwin('cd E:AllProjectGit/B100KSOC ; rm -rf *; repo sync')
-print cygwin('cd E:AllProjectGit/B100KSOC-gas ; rm -rf *; repo sync')
-print cygwin('cd E:AllProjectGit/C11IOC ; rm -rf *; repo sync')
-print cygwin('cd E:AllProjectGit/C11SOC ; rm -rf *; repo sync')
-print cygwin('cd E:AllProjectGit/C51E ; rm -rf *; repo sync')
-print cygwin('cd E:AllProjectGit/C4053FIOC ; rm -rf *; repo sync')
-print cygwin('cd E:AllProjectGit/C4053FSOC ; rm -rf *; repo sync')'''
 
+# print cygwin('cd E:AllProjectGit/B100KIOC ; rm -rf *; repo sync')
+# print cygwin('cd E:AllProjectGit/B100KIOC-gas ; rm -rf *; repo sync')
+# print cygwin('cd E:AllProjectGit/B100KSOC ; rm -rf *; repo sync')
+# print cygwin('cd E:AllProjectGit/B100KSOC-gas ; rm -rf *; repo sync')
+# print cygwin('cd E:AllProjectGit/C11IOC ; rm -rf *; repo sync')
+# print cygwin('cd E:AllProjectGit/C11SOC ; rm -rf *; repo sync')
+# print cygwin('cd E:AllProjectGit/C51E ; rm -rf *; repo sync')
+# print cygwin('cd E:AllProjectGit/C4053FIOC ; rm -rf *; repo sync')
+# print cygwin('cd E:AllProjectGit/C4053FSOC ; rm -rf *; repo sync')
+
+#cygwin('cd E:AllProjectGit/'+'C51E'+' ; rm -rf *; repo sync') 
+# 因为有些项目会卡住，所以暂时先不用下面的for循环了
 for filename in os.listdir(r'E:\AllProjectGit'):
-    if filename not in ['GitProject','Pro Git 2nd Edition']:
+    if filename not in ['GitProject','Pro Git 2nd Edition']:#,'B100KIOC','B100KIOC-gas','B100KSOC','B100KSOC-gas','C11IOC']:
         print filename
-        cygwin('cd E:AllProjectGit/'+filename+' ; repo help') 
+        cygwin('cd E:AllProjectGit/'+filename+' ; rm -rf *; repo sync') 
 
 print '---------------------------------------------------------------------------\n repo sync done!\n\n'    
 #IOC git project update
@@ -69,14 +77,14 @@ print '-------------------------------------------------------------------------
 for filename in os.listdir(r'E:\AllProjectGit\GitProject'):
     if filename not in ['SOC', 'Tool']: #'' and filename != '': #and filename != 'common_base' and filename != 'QNX':
         print filename
-        cygwin('cd E:AllProjectGit/GitProject/'+filename+' ; git branch -av')
+        cygwin('cd E:AllProjectGit/GitProject/'+filename+' ; git fetch origin')
    
 print '---------------------------------------------------------------------------\nIOC git fetch done!\n\n'    
 print '---------------------------------------------------------------------------\nSOC git fetch start!\n\n' 
 
 for filename in os.listdir(r'E:\AllProjectGit\GitProject\SOC'): 
     print filename
-    cygwin('cd E:AllProjectGit/GitProject/SOC/'+filename+' ; pwd ; git branch -av')
+    cygwin('cd E:AllProjectGit/GitProject/SOC/'+filename+' ; git fetch origin')
          
 #cygwin('cd E:AllProjectGit/GitProject/QNX ; pwd; git branch -av')
     
@@ -86,8 +94,12 @@ for filename in os.listdir(r'E:\AllProjectGit\GitProject\SOC'):
     #pass
 print '---------------------------------------------------------------------------\nSOC git fetch done!\n\n'    
     
-    
-print '---------------------------------------------------------------------------\ntask done!\n\n'
+TaskAftertime = time.time()    
+print '---------------------------------------------------------------------------\ntask done!\nTask Total Time is '\
++str(TaskAftertime-TaskStarttime)+'s'
+
+
+
 # print cygwin('pwd')
 # print cygwin('ls -l')
 # print cygwin('repo sync')
